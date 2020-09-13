6a7ac695428c1423e9522aa7a81bb52fa6e2befe
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2020-09-13 16:37:52 +0200

    stylish

--- plot.py
+++ plot.py
@@ -12,6 +12,7 @@ from pprint import pprint
 # FIXME: 3d
 # FIXME: sorted by score
 # FIXME: time/loss (power line)
+# FIXME: [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[generacja tabelek]]]]]]]]
 ###############################
 
 def slugify(text):
@@ -61,11 +62,6 @@ def read_file(path=None, x_name=None, y_name=None):
             Y.append(float(log[y_name][i]))
     return X, Y, log
 
-
-def fill_between(X, Y, color="blue", alpha=0.05, factor=1):
-    sigma = factor * np.array(Y).std(axis=0)  # ls = '--'
-    ax.fill_between(X, Y + sigma, Y - sigma, facecolor=color, alpha=alpha)
-
 def apply_SOTA(x_name=None, y_name=None, sort=False):
     # for [JFA]
     path = "results/jfa.json"
@@ -85,6 +81,7 @@ def apply_SOTA(x_name=None, y_name=None, sort=False):
     if y_name == "time":
         plt.plot(x, [1]*len(x), label="brutforce", color="red", linestyle='solid')
         ax.fill_between(x, [1]*len(x), 0, facecolor='red', alpha=0.2)
+    ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10, alpha=0.1)
 
 def globlog():
     vec = []
@@ -95,8 +92,9 @@ def globlog():
 
 ### FIGURE (1): underfitting ###
 
-# FIXME: funny name?
-
+# FIXME:
+# - u gory: segmenty z np. 64x64, 128x128
+# - na dole: jakie `seeds` jakie `rho`
 def apply_domain(x=None):
     # FIXME: dodac umiejetnosc pomijania niektorych? jak jest gesto?
     path = "results/domain.json"
@@ -110,8 +108,19 @@ def apply_domain(x=None):
         domain_str.append(f"{d['shape'][0]}x{d['shape'][1]}\nseeds={d['num']}\n$\\rho$={q}")
         # domain_str.append(f"{d['shape'][0]}x{d['shape'][1]}\n{d['num']}")
     plt.xticks(x, domain_str, rotation='horizontal', fontsize=2)
+    # plt.xticks(x, domain_str, rotation='horizontal', fontsize=2)
+    # import matplotlib.ticker as mticker
+    # myLocator = mticker.MultipleLocator(2)
+    # ax.xaxis.set_major_locator(myLocator)
+    #ax.tick_params(axis="x", bottom=True, top=True, labelbottom=True, labeltop=True)
+    #plt.setp([tick.label1 for tick in ax.xaxis.get_major_ticks()], rotation='horizontal', fontsize=2)
+    #plt.setp([tick.label1 for tick in ax.xaxis.get_major_ticks()], rotation='horizontal', fontsize=2)
 
 COLORS = ['g', 'b', 'm', 'orange']
+
+################################################################################
+# FIGURES
+################################################################################
 
 with figure("time", prefix=1):
     # FIXME: gray -----> alpha change on i?
@@ -121,6 +130,8 @@ with figure("time", prefix=1):
             plt.plot(x, y, label=log["name"], color=COLORS[i], linestyle='--')
         else:
             plt.plot(x, y, label=log["name"], color="gray", alpha=0.1, linestyle='solid')
+        if i >= 11:
+            break
 
     apply_SOTA(x_name=None, y_name="time", sort=False)
     apply_domain(x)
@@ -136,6 +147,8 @@ with figure("loss", prefix=2):
             plt.plot(x, y, label=log["name"], color=COLORS[i], linestyle='--')
         else:
             plt.plot(x, y, label=log["name"], color="gray", alpha=0.1, linestyle='solid')
+        if i >= 11:
+            break
 
     apply_SOTA(x_name=None, y_name="loss", sort=False)
     apply_domain(x)
@@ -151,6 +164,8 @@ with figure("score", prefix=3):
             plt.plot(x, y, label=log["name"], color=COLORS[i], linestyle='--')
         else:
             plt.plot(x, y, label=log["name"], color="gray", alpha=0.1, linestyle='solid')
+        if i >= 11:
+            break
 
     apply_SOTA(x_name=None, y_name="score", sort=False)
     apply_domain(x)
@@ -168,118 +183,16 @@ with figure("power", prefix=4):
             plt.plot(x, y, label=log["name"], color=COLORS[i], linestyle='--')
         else:
             plt.plot(x, y, label=log["name"], color="gray", alpha=0.1, linestyle='solid')
+        if i >= 11:
+            break
 
     apply_SOTA(x_name=None, y_name="score", sort=True)    
-    apply_domain(x)
+    # apply_domain(x) ???
 
     plt.ylabel("score")
     plt.xlabel("case (unordered)")
     plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
 
-"""
-with figure("test accuracy (without augment)", prefix=1):
-    x1, y1 = read_file("decay=1e-4/log_EfficientNet_batchboost_1")
-    plt.plot(x1, y1, label="boostbatch (alpha=1.0)", color="darkred")
 
-    x1, y1 = read_file("decay=1e-4/log_EfficientNet_batchboost_3")
-    plt.plot(x1, y1, label="boostbatch (alpha=0.4)", color="red")
+# FIXME: 3d wykres?
 
-    x2, y2 = read_file("decay=1e-4/log_EfficientNet_mixup_1")
-    plt.plot(x2, y2, label="mixup (alpha=1.0)", color="darkblue")
-
-    x2, y2 = read_file("decay=1e-4/log_EfficientNet_mixup_3")
-    plt.plot(x2, y2, label="mixup (alpha=0.4)", color="blue")
-
-    x3, y3 = read_file("decay=1e-4/log_EfficientNet_baseline_13")
-    plt.plot(x3, y3, label="baseline", color="black")
-
-    plt.ylabel("accuracy")
-    plt.xlabel("epoch")
-    # plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-
-with figure("loss train (without augment)", prefix=1):
-    x1a, y1a = read_file("decay=1e-4/log_EfficientNet_batchboost_1", col=1)
-    plt.plot(x1a, y1a, label="boostbatch (alpha=1.0)", color="darkred")
-
-    x1b, y1b = read_file("decay=1e-4/log_EfficientNet_batchboost_3", col=1)
-    plt.plot(x1b, y1b, label="boostbatch (alpha=0.4)", color="red")
-
-    fill_between(x1a,
-                 np.mean([y1a, y1b], axis=0),
-                 color="red",
-                 factor=1,
-                 alpha=0.1)
-
-    x2a, y2a = read_file("decay=1e-4/log_EfficientNet_mixup_1", col=1)
-    plt.plot(x2a, y2a, label="mixup (alpha=1.0)", color="darkblue")
-
-    x2b, y2b = read_file("decay=1e-4/log_EfficientNet_mixup_3", col=1)
-    plt.plot(x2b, y2b, label="mixup (alpha=0.4)", color="blue")
-
-    fill_between(x2a,
-                 np.mean([y2a, y2b], axis=0),
-                 color="blue",
-                 factor=1,
-                 alpha=0.1)
-
-    x3, y3 = read_file("decay=1e-4/log_EfficientNet_baseline_13", col=1)
-    plt.plot(x3, y3, label="baseline", color="black")
-
-    plt.ylabel("loss")
-    plt.xlabel("epoch")
-    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-
-### FIGURE (2): overfitting (compirason to mixup) ###
-
-with figure("test accuracy (with augment)", prefix=2):
-    x1a, y1a = read_file("decay=1e-5/log_EfficientNet_batchboost_2")
-    plt.plot(x1a, y1a, label="boostbatch (alpha=1.0)", color="darkred")
-
-    x1b, y1b = read_file("decay=1e-5/log_EfficientNet_batchboost_4")
-    plt.plot(x1b, y1b, label="boostbatch (alpha=0.4)", color="red")
-
-    fill_between(x1a,
-                 np.mean([y1a, y1b], axis=0),
-                 color="red",
-                 factor=0.5,
-                 alpha=0.1)
-
-    x2a, y2a = read_file("decay=1e-5/log_EfficientNet_mixup_2")
-    plt.plot(x2a, y2a, label="mixup (alpha=1.0)", color="darkblue")
-
-    x2b, y2b = read_file("decay=1e-5/log_EfficientNet_mixup_4")
-    plt.plot(x2b, y2b, label="mixup (alpha=0.4)", color="blue")
-
-    fill_between(x2a,
-                 np.mean([y2a, y2b], axis=0),
-                 color="blue",
-                 factor=0.5,
-                 alpha=0.1)
-
-    # x3, y3 = read_file("decay=1e-5/log_EfficientNet_baseline_24")
-    # plt.plot(x3, y3, label="baseline", color="black")
-
-    plt.ylabel("accuracy")
-    plt.xlabel("epoch")
-    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-
-with figure("train accuracy (with augment)", prefix=2):
-    x1, y1 = read_file("decay=1e-5/log_EfficientNet_batchboost_2", col=3)
-    plt.plot(x1, y1, label="boostbatch (alpha=1.0)", color="darkred")
-
-    x1, y1 = read_file("decay=1e-5/log_EfficientNet_batchboost_4", col=3)
-    plt.plot(x1, y1, label="boostbatch (alpha=0.4)", color="red")
-
-    x2, y2 = read_file("decay=1e-5/log_EfficientNet_mixup_2", col=3)
-    plt.plot(x2, y2, label="mixup (alpha=1.0)", color="darkblue")
-
-    x2, y2 = read_file("decay=1e-5/log_EfficientNet_mixup_4", col=3)
-    plt.plot(x2, y2, label="mixup (alpha=0.4)", color="blue")
-
-    # x3, y3 = read_file("decay=1e-5/log_EfficientNet_baseline_24", col=3)
-    # plt.plot(x3, y3, label="baseline", color="black")
-
-    plt.ylabel("accuracy")
-    plt.xlabel("epoch")
-    # plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
-"""
